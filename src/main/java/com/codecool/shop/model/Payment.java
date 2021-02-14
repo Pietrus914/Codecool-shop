e8e21c45c8ab5354b70a68f634335f561cd98845
Pietrus914 <pietrus914@gmail.com>
2021-02-14 16:22:09 +0100

    adding logic for payment(initializer and paymentcontroler)

--- src/main/java/com/codecool/shop/model/Payment.java
+++ src/main/java/com/codecool/shop/model/Payment.java
@@ -1,13 +1,26 @@
 package com.codecool.shop.model;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Payment extends  BaseModel{
 
 
     private String name;
+    private List<Payment> payments;
 
     public Payment(String name) {
         super(name);
         this.name = name;
+        this.payments = new ArrayList<>();
+    }
+
+    public List<Payment> getPayments() {
+        return payments;
+    }
+
+    public void setPayments(List<Payment> payments) {
+        this.payments = payments;
     }
 
     public String getName() {
